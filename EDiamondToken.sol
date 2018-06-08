65886588107a8d994eff49122bae30fba845f1e8
eDiamond <developer@ediamond.love>
2018-06-08 16:14:10 +0800

    fix

--- EDiamondToken.sol
+++ EDiamondToken.sol
@@ -313,12 +313,12 @@ contract EDiamondToken is StandardToken, Ownable {
 
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(judgeEnableForTransfer(msg.sender, _value));
-        super.transfer(_to, _value);
+        return super.transfer(_to, _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         require(judgeEnableForTransfer(_from, _value));
-        super.transferFrom(_from, _to, _value);
+        return super.transferFrom(_from, _to, _value);
     }
 
 }
